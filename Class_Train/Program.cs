1fdc74adec7cf440594a04d10c646f65380c53cf
Alex <sakalinski95@gmail.com>
2023-07-31 16:46:54 +0300

    Добавьте файлы проекта.

---
+++ Class_Train/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Class_Train
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Train train1 = new Train();
+            Train train2 = new Train("2");
+            Train train3 = new Train("3", 25, "Minsk", new double[] { 2, 3, 2, 1, 5 });
+            Train train4 = new Train("4", 20, "Gomel", new double[] { 2, 3, 7, 9, 4 });
+            Console.WriteLine(train1.ToString());
+            Console.WriteLine(train2.ToString());
+            Console.WriteLine(train3.ToString());
+            Console.WriteLine(train4.ToString());
+            Console.WriteLine("Проверка мощности");
+            Console.Write("1. ");
+            if (train1.IsPowerfull(train1.Power, train1.Massa) == true) Console.WriteLine("Мощности достаточно");
+            else Console.WriteLine("Мощности не достаточно");
+            Console.Write("2. ");
+            if (train2.IsPowerfull(train2.Power, train2.Massa) == true) Console.WriteLine("Мощности достаточно");
+            else Console.WriteLine("Мощности не достаточно");
+            Console.Write("3. ");
+            if (train3.IsPowerfull(train3.Power, train3.Massa) == true) Console.WriteLine("Мощности достаточно");
+            else Console.WriteLine("Мощности не достаточно");
+            Console.Write("4. ");
+            if (train1.IsPowerfull(train4.Power, train4.Massa) == true) Console.WriteLine("Мощности достаточно");
+            else Console.WriteLine("Мощности не достаточно");
+            Console.WriteLine("Тяжелейший из 2-х между 3-м и 4-м");
+            if (train3.Heavier(train4) == true) Console.WriteLine("3 тяжелее 4");
+            else Console.WriteLine("4 тяжелее 3");
+            Console.WriteLine("Тяжелейший из 3-х");
+            Console.WriteLine(Train.HeavierFrom3(train2, train3, train4));
+            Console.WriteLine("\nДве переменные ссылаются на один объект:");
+            Train train = new("5", 25, "Moskva", new double[] { 1, 2, 5, 1, 5 }); ;
+            Console.WriteLine(train.ToString());
+            Train trainNew = train;
+            trainNew.Power = 14;
+            Console.WriteLine(train.ToString());
+        }
+    }
+}
---
+++ Class_Train/Train.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Class_Train
+{
+    internal class Train
+    {
+        string? number;
+        double power;
+        string? destination;
+        double[] massa = new double[5];
+
+        public Train() : this("1")
+        { }
+        public Train(string number) : this(number, 1, "unknown", new double[] { 0, 0, 0, 0, 0 })
+        { }
+        public Train(string number, double power, string destination, double[] massa)
+        {
+            Number = number;
+            Power = power;
+            Destination = destination;
+            Massa = massa;
+        }
+
+        public string Number { get; set; }
+        public double Power
+        {
+            get => power;
+            set
+            {
+                if (value < 0) power = 0;
+                else power = value;
+            }
+        }
+        public string Destination { get; set; }
+        public double[] Massa
+        {
+            get => massa;
+            set
+            {
+                for (int i = 0; i < massa.Length; i++) massa[i] = 0;
+                for (int i = 0; i < value.Length && i < massa.Length; i++)
+                {
+                    if (value[i] < 0) massa[i] = 0;
+                    else massa[i] = value[i];
+                }
+            }
+        }
+
+        public double FullMassa(params double[] massa)
+        {
+            double summa = 0;
+            for (int i = 0; i < massa.Length; i++)
+            {
+                summa += massa[i];
+            }
+            return summa;
+        }
+
+        public bool IsPowerfull(double power, params double[] massa)
+        {
+            if (power >= FullMassa(massa)) return true;
+            else return false;
+        }
+
+        public bool Heavier(Train other)
+        {
+            if (this.FullMassa() > other.FullMassa()) return true;
+            else return false;
+        }
+
+        public static Train HeavierFrom3(Train train1, Train train2, Train train3)
+        {
+            if ((train1.FullMassa() > train2.FullMassa()) && (train1.FullMassa() > train3.FullMassa()))
+            {
+                return train1;
+            }
+            else if ((train2.FullMassa() > train1.FullMassa()) && (train2.FullMassa() > train3.FullMassa()))
+            {
+                return train2;
+            }
+            else return train3;
+        }
+
+        public override string? ToString()
+        {
+            return $"Номер {Number} мощность (макс. допустимая масса состава в т.) {Power} назначение {Destination} общая масса вагонов в т. {FullMassa(Massa)}";
+        }
+    }
+}
